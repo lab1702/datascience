1ea99f32a068a7a08412967f65907a2d11ad2748
lab1702 <lab1702@gmail.com>
2020-11-21 17:56:00 -0500

    Add files via upload

---
+++ 01_BUILD.bat
@@ -0,0 +1,2 @@
+docker build -t datascienceimage --pull --no-cache .
+docker system prune -f
---
+++ 02_RUN.bat
@@ -0,0 +1,1 @@
+docker run --rm -ti -p 127.0.0.1:8787:8787 -p 127.0.0.1:8888:8888 -e DISABLE_AUTH=true datascienceimage
